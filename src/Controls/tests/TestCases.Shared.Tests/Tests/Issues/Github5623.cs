af1a8e963374519a580464d50c20a14c146d783b
Gerald Versluis <gerald.versluis@microsoft.com>
2024-08-12 14:00:24 +0200

    Add first batch of XamUITests
    Build fix
    
    Fix some tests, add some tests
    
    Fix build
    
    More tests + fixes
    
    Last one
    
    Lets gooo
    
    Please fix
    
    Really fix build
    
    Really fix build
    
    Fix build
    
    First batch to Appium
    
    Fix build
    
    Add more tests
    
    Add more tests
    
    Fixed build issues
    
    Add more tests
    
    Add more tests
    
    Fix and disable some tests
    
    Fix build
    
    Fix and add tests
    
    Delete obsolete test & fix build
    
    Add more tests
    
    Fix more tests
    
    Fix more tests
    
    Fix build error
    
    Add missing category
    
    Maybe fix build?
    
    Fix tests
    
    Add more tests
    
    More tests
    
    More tests

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Github5623.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Github5623 : _IssuesUITest
+{
+	public Github5623(TestDevice testDevice) : base(testDevice)
+	{
+	}
+
+	public override string Issue => "Button Released not being triggered";
+
+	// [Test]
+	// [Category(UITestCategories.CollectionView)]
+	// [FailsOnIOS]
+	// public void CollectionViewInfiniteScroll()
+	// {
+	// 	RunningApp.WaitForElement("CollectionView5623");
+
+	// 	var colView = RunningApp.Query("CollectionView5623").Single();
+
+	// 	AppResult[] lastCellResults = null;
+
+	// 	RunningApp.QueryUntilPresent(() =>
+	// 	 {
+	// 		 RunningApp.DragCoordinates(colView.Rect.CenterX, colView.Rect.Y + colView.Rect.Height - 50, colView.Rect.CenterX, colView.Rect.Y + 5);
+
+	// 		 lastCellResults = RunningApp.Query("99");
+
+	// 		 return lastCellResults;
+	// 	 }, 100, 1);
+
+	// 	Assert.IsTrue(lastCellResults?.Any() ?? false);
+	// }
+}
